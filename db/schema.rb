fe5795154c2682b3f0a30d8de5af43d5dbcf0e97
kounishi <g854t35krb@i.softbank.jp>
2016-03-30 08:03:07 +0000

    change db postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160326095136) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "kakeibos", force: :cascade do |t|
     t.string   "incomeitem"
     t.integer  "incomeprice"
